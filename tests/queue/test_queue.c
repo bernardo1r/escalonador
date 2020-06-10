6b3801f2d4713cfd9cc8a84aee7e76be7492c3f7
bernardo <bernardo2010m@hotmail.com>
2020-06-10 16:49:48 -0300

    criada pasta dentro da pasta de teste para fila

--- tests/queue/test_queue.c
+++ tests/queue/test_queue.c
@@ -6,6 +6,19 @@ void list_arr_print(void *arg)
     printf("%d ", *(int *)arg);
 }
 
+void *multiply_2(void *val)
+{
+    int *number = val;
+    *number *= 2;
+    return number;
+}
+
+void *subtract_1(void *val)
+{
+    int *number = val;
+    *number -= 1;
+    return number;
+}
 
 int main(void)
 {
@@ -26,6 +39,20 @@ int main(void)
         queue_push(q, &arr[i]);
         queue_print(q, list_arr_print);
     }
+
+    puts("\nMultiply all queue members by 2:");
+    queue_map(q, multiply_2);
+    queue_print(q, list_arr_print);
+
+    puts("\nSubtract all queue members by 1:");
+    queue_map(q, subtract_1);
+    queue_print(q, list_arr_print);
+
+    puts("\nSee first element in the queue");
+    printf("%d\n", *(int *)queue_seeFirst(q));
+    queue_print(q, list_arr_print);
+
     queue_free(q);
+
     return 0;
 }
